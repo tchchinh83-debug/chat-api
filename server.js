b8cee16b253f375518e8bc50917414449ddd3517
Cao Hoai Chinh  Truong <chinhtch.quangngai@vss.local>
2026-03-18 17:06:16 +0700

    fix api version

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/chat", async (req, res) => {
     const message = req.body.message;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
