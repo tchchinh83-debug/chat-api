bdb651a7b5cf4c95d2cc489da8e8fb4d341d3d85
Cao Hoai Chinh  Truong <chinhtch.quangngai@vss.local>
2026-03-18 17:10:00 +0700

    fix model to gemini-pro

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/chat", async (req, res) => {
     const message = req.body.message;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
